90210a99c9476f32f2086660fe58070a345be178
SkyR1seR <56387043+SkyR1seR@users.noreply.github.com>
2020-06-23 11:39:57 +0400

    ПМ_02_пункт_3

---
+++ Выполненное задание/ПМ_02_модуль.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pifagor
+{
+    public static class Pif
+    {
+        /// <summary>
+        /// Метод вывода Пифагоровых чисел из промежутка
+        /// </summary>
+        /// <param name="x">Начало промежутка</param>
+        /// <param name="y">Конец промежутка</param>
+        /// <returns></returns>
+        public static string Number(int x, int y)
+        {
+            string row = "";
+            {
+                for (int c = x; c <= y; ++c)
+                    for (int b = x; b <= c; ++b)
+                        for (int a = x; a <= b; ++a)
+                            if (a * a + b * b == c * c)  row += (a,b,c) + ", ";
+            }
+            return row;
+        }
+    }
+}
